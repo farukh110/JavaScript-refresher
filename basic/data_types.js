9c11c135615767d821a1cc4b5f2744525a120cdf
Farukh Sajjad <farukhsajjad110@gmail.com>
2023-12-16 16:50:01 +0000

    data types

---
+++ basic/data_types.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3);
+
+// premitive data types 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// null
+// symbol => unique
+
+// none premitive data types
+
+// object
--- basic/index.js
+++ basic/index.js
@@ -1,1 +1,10 @@
-console.log('test');
+const accountId = 125678765;
+let email = 'farukhsajjad110@gmail.com';
+
+console.table([accountId, email]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
