942629817209f5a25697b1cab501699c71270c7a
Farukh Sajjad <farukhsajjad110@gmail.com>
2023-12-17 09:16:39 +0000

    Stack and heap

--- basic/data_types_summary.js
+++ basic/data_types_summary.js
@@ -13,3 +13,33 @@
 // const anotherId = Symbol('123');
 
 // console.log(id === anotherId);
+
+// ---------------------------------------------------------------------------------
+
+// memory
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// in primitive copy of data will change
+// in non primitive reference of data
+
+let name = 'Farukh Sajjad';
+
+let another_name = name;
+
+another_name = 'Oan Sajjad';
+
+console.log('name: ',name);
+console.log('another_name: ', another_name);
+
+let user_one = {
+    email: 'oan@gmail.com',
+    nic: 1213213232323
+};
+
+let user_two = user_one;
+
+user_two.email = 'farukh.sajjad@trax.pk'
+
+console.log('user_one: ', user_one.email);
+console.log('user_two: ', user_two.email);
