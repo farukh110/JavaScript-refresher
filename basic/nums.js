3d0d66d2f0a403bd837554d86138dbefc3c551a5
Farukh Sajjad <farukhsajjad110@gmail.com>
2023-12-23 12:38:56 +0000

    numbers

---
+++ basic/nums.js
@@ -0,0 +1,30 @@
+const score = 400;
+
+console.log(score);
+
+// const balance = new Number(600);
+
+// console.log(balance);
+
+// const otherNumber = 123.8966;
+
+// console.log(otherNumber.toPrecision(3));
+
+// console.log(Math.abs(4));
+
+// console.log(Math.round(4.3));
+
+// console.log(Math.round(4.5));
+
+// console.log(Math.ceil(4.6));
+
+// console.log(Math.floor(4.6));
+
+// console.log(Math.random() * 10 + 1);
+
+// console.log(Math.floor(Math.random() * 10));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
