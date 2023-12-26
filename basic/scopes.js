bf7836fb6c5460a1c7f66bece7a8a75073333e3a
Farukh Sajjad <farukhsajjad110@gmail.com>
2023-12-26 12:13:34 +0000

    continue scopes

--- basic/scopes.js
+++ basic/scopes.js
@@ -22,15 +22,58 @@
 // console.log(b);
 // console.log(c);
 
-let a = 300;
+// let a = 300;
 
-if (true) {
+// if (true) {
 
-    let a = 10;
-    const b = 20;
-    var c = 30;
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
 
-    // console.log('inner a: ',a);
+//     // console.log('inner a: ',a);
+// }
+
+// console.log('outer a: ',a);
+
+// const one = () => {
+
+//     const username = 'Farukh Sajjad';
+
+//     const two = () => {
+
+//         const website = 'youtube';
+//         console.log(username);
+//     }
+
+//     // console.log('website: ', website);
+//     // two();
+// }
+
+// one();
+
+// if(true) {
+//     const username = 'farukh sajjad';
+
+//     if (username === 'farukh sajjad') {
+
+//         const website = 'youtube.com';
+//         console.log(username + website);
+//     }
+
+//     console.log(website);
+// }
+
+// console.log(username);
+
+console.log(addone(5));
+
+function addone(num) {
+    return num + 1;
 }
 
-console.log('outer a: ',a);
+console.log(addTwo(5));
+
+const addTwo = (num) => {
+
+    return num + 2;
+} 
