03925a9626849e1e0bbe0c34518874557bd141f8
Farukh Sajjad <farukhsajjad110@gmail.com>
2024-01-08 18:52:04 +0000

    loop

---
+++ basic/loops.js
@@ -0,0 +1,18 @@
+// let myArray = ['flash', 'batman', 'superman'];
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+for (let index = 0; index <= 20; index++) {
+    
+    if(index === 5) {
+
+        console.log('Detected 5');
+        break;
+        // continue;
+    }
+    
+    console.log(`Value of i is ${index}`);
+}
