f10836cbd73a9ee3cdcd1df8a52fc0c7389a8fc8
Farukh Sajjad <farukhsajjad110@gmail.com>
2024-01-11 18:44:10 +0000

    for each, filter

---
+++ basic/loops2.js
@@ -0,0 +1,65 @@
+// ["", "", ""];
+// [{}, {}, {}];
+
+// const arr = [1, 2, 3, 4, 5];
+
+// for(const num of arr) {
+
+//     console.log(num);
+// }
+
+// const greetings = "Farukh Sajjad";
+
+// for(const text of greetings) {
+ 
+//     console.log(text)
+// }
+
+// const gameObject = {
+
+//     'game1': 'NFS',
+//     'game2': 'Spiderman'
+
+// };
+
+// for(const key in gameObject) {
+ 
+//     console.log(`key: ${key} and value: ${gameObject[key]}`)
+
+// }
+
+// const stack = ['M','E', 'R','N'];
+
+// stack.forEach((item, index, arr) => {
+
+//     console.log(item, index, arr);
+// });
+
+
+// const values = stack.forEach((item) => {
+
+//     // console.log(item);
+//     return item;
+// });
+
+// console.log(values);
+
+// const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// // const nums = myNumbers.filter((num) => {
+// //     return num > 4
+// // });
+
+// // console.log(nums);
+
+// const newItems = [];
+
+// myNumbers.forEach((item) => {
+
+//     if(item > 4) {
+
+//         newItems.push(item);
+//     }
+// });
+
+// console.log(newItems);
